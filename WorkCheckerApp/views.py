158696038d5c9de3c12ece45c4e7a5fe39a7e43d
DRON12261 <andonovec2000@mail.ru>
2021-10-18 00:17:58 +0500

    Update3

--- WorkCheckerApp/views.py
+++ WorkCheckerApp/views.py
@@ -3,31 +3,170 @@ from django.core.files.storage import FileSystemStorage
 from django.http import FileResponse
 from .models import ErrorDoc
 import os.path
+import lxml
+import zipfile
+import re
+from bs4 import BeautifulSoup
 
 class DocError():
     number = 0
     errorType = ""
     errorDisc = ""
-    pageNum = 0
-    lineNum = 0
-    solution = ""
+    chapter = ""
+    text = ""
 
-    def __init__(self, number, errorType, errorDisc, pageNum, lineNum, solution):
+    def __init__(self, number, errorType, errorDisc, chapter, text):
         self.number = number
         self.errorType = errorType
         self.errorDisc = errorDisc
-        self.pageNum = pageNum
-        self.lineNum = lineNum
-        self.solution = solution
+        self.chapter = chapter
+        self.text = text
 
     def __str__(self):
-        return '%s | %s | %s | %s | %s | %s' % (self.number, self.errorType, self.errorDisc, self.pageNum, self.lineNum, self.solution)
+        return '%s | %s | %s | %s | %s' % (self.number, self.errorType, self.errorDisc, self.chapter, self.text)
 
     def serialize(self):
         return self.__dict__
 
-def parse(doc_path, doc_type, wct_path):
-    print(doc_path + '\n' + doc_type + '\n' + wct_path)
+def parse(request, doc_path, html_path, doc_type, wct_path):
+    errornum = 0
+    if doc_type == 1:
+        with open(wct_path, "r") as f:
+            f.readline()
+            request.session['FontSize'] = float(f.readline())
+            request.session['FontName'] = f.readline()
+            request.session['ParIndent'] = float(f.readline())
+            request.session['TextAlign'] = f.readline()
+            request.session['FLeft'] = float(f.readline())
+            request.session['FRight'] = float(f.readline())
+            request.session['FUp'] = float(f.readline())
+            request.session['FDown'] = float(f.readline())
+            request.session['LineSpace'] = float(f.readline())
+            f.close()
+    else:
+        request.session['FontSize'] = 14
+        request.session['FontName'] = 'Times New Roman'
+        request.session['ParIndent'] = 36
+        request.session['TextAlign'] = 'justify'
+        request.session['FLeft'] = 10
+        request.session['FRight'] = 10
+        request.session['FUp'] = 10
+        request.session['FDown'] = 10
+        request.session['LineSpace'] = 12
+
+    with open(html_path, "r") as f:
+        contents = f.read()
+        soup = BeautifulSoup(contents, 'lxml')
+        tags = soup.find_all()
+        pars = soup.find_all("p")
+        chapters = [[soup.new_tag("div")]]
+        chapters[0][0].string = "[Начало документа]"
+        currentChapter = 0
+
+        for tag in tags:
+            if tag.name == "h1" and re.search('\S', tag.span.text) != None:
+                #print(tag.span.text)
+                chapters.append([])
+                currentChapter += 1
+                chapters[currentChapter].append(tag)
+
+            if tag.name == "p" and re.search('\S', tag.text) != None:
+                #print(tag.span.text)
+                bold = tag.b
+                if bold != None and bold.span != None and re.search('\S', tag.span.text) != None:
+                    parAtr = tag.attrs["style"]
+                    parStyles = re.split(";|,", parAtr.replace('\n', ""))
+                    for parStyle in parStyles:
+                        if parStyle.find('text-align:') != -1:
+                            ta = parStyle[parStyle.find(':') + 1:]
+                            if ta == "center":
+                                chapters.append([])
+                                currentChapter += 1
+                                chapters[currentChapter].append(tag)
+                spanChild = None
+                for child in tag.children:
+                    if child.name == "span":
+                        spanChild = child
+                if spanChild != None:
+                    spanAtr = spanChild.attrs["style"]
+                    spanStyles = re.split(";|,", spanAtr.replace('\n', ""))
+                    for spanStyle in spanStyles:
+                        if spanStyle.find("font-family:") != -1:
+                            ff = spanStyle[spanStyle.find(':') + 1:]
+                            ff = ff.replace('\"', "")
+                            if ff != request.session['FontName']:
+                                errornum += 1
+                                request.session['ErrorList'].append(DocError(errornum, "Неподходящий шрифт", 'Должен быть шрифт "' + request.session['FontName'] + '". У вас стоит шрифт "' + ff + '".', chapters[currentChapter][0].text, tag.text).serialize())
+                            #print(ff)
+                            #print(spanStyles)
+                        if spanStyle.find("font-size:") != -1:
+                            fs = spanStyle[spanStyle.find(':') + 1:]
+                            fs = fs[:fs.find('.')]
+                            if float(fs) != float(request.session['FontSize']):
+                                #print(spanChild)
+                                #print(tag)
+                                errornum += 1
+                                request.session['ErrorList'].append(DocError(errornum, "Неподходящий размер шрифта", 'Размер шрифта должен быть равен "' + str(request.session['FontSize']) + '". У вас размер шрифта равен "' + str(fs) + '".', chapters[currentChapter][0].text, tag.text).serialize())
+                            #print(fs)
+
+                if "style" in tag.attrs:
+                    parAtr = tag.attrs["style"]
+                    parStyles = re.split(";|,", parAtr.replace('\n', ""))
+
+                    for parStyle in parStyles:
+                        if parStyle.find('margin-top:') != -1:
+                            mt = parStyle[parStyle.find(':') + 1:]
+                            mt = mt[:mt.find('.')]
+                            if float(mt) != request.session['LineSpace'] and float(mt) != 0:
+                                errornum += 1
+                                request.session['ErrorList'].append(DocError(errornum, "Неподходящий межстрочный интервал", 'Межстрочный интервал должен быть равен "' + str(request.session['LineSpace']) + '". У вас межстрочный интервал равен "' + str(mt) + '".', chapters[currentChapter][0].text, tag.text).serialize())
+                            # print("mt:" + mt)
+
+                        if parStyle.find('text-align:') != -1:
+                            ta = parStyle[parStyle.find(':') + 1:]
+                            if ta != request.session['TextAlign']:
+                                textAl = ""
+                                textAlTrue = ""
+                                if ta == "justify":
+                                    textAl = "по ширине"
+                                elif ta == "center":
+                                    textAl = "по центру"
+                                elif ta == "left":
+                                    textAl = "по левой стороне"
+                                elif ta == "right":
+                                    textAl = "по правой стороне"
+                                else:
+                                    textAl = "неизвестным образом"
+                                if request.session['TextAlign'] == "justify":
+                                    textAlTrue = "по ширине"
+                                elif request.session['TextAlign'] == "center":
+                                    textAlTrue = "по центру"
+                                elif request.session['TextAlign'] == "left":
+                                    textAlTrue = "по левой стороне"
+                                elif request.session['TextAlign'] == "right":
+                                    textAlTrue = "по правой стороне"
+                                else:
+                                    textAlTrue == "неизвестным образом"
+                                errornum += 1
+                                request.session['ErrorList'].append(DocError(errornum, "Неподходящее выравнивание текста", 'Выравнивание текста должно быть выставлено ' + textAlTrue + '. У вас оно выставлено ' + textAl + '.', chapters[currentChapter][0].text, tag.text).serialize())
+                            # print("ta:" + ta)
+                            
+                        if parStyle.find('text-indent:') != -1:
+                            ti = parStyle[parStyle.find(':') + 1:]
+                            if ti.find('.') != -1:
+                                ti = ti[:ti.find('.')]
+                            if ti.find('cm') != -1:
+                                ti = ti[:ti.find('cm')]
+                            if float(ti) != request.session['ParIndent'] and float(ti) != 0 and float(ti) != -18:
+                                errornum += 1
+                                request.session['ErrorList'].append(DocError(errornum, "Неподходящий абзацный отступ", 'Абзацный отступ должен быть равен "' + str(request.session['ParIndent']) + '". У вас абзацный отступ равен "' + str(ti) + '".', chapters[currentChapter][0].text, tag.text).serialize())
+                            # print("ti:" + ti)
+        #print(len(chapters))
+        for ch in chapters:
+            #print(ch[2])
+            #print("---------------------------------------------------------------------------")
+            pass
+    #print(doc_path + '\n' + html_path + '\n' + doc_type + '\n' + wct_path)
 
 def checker(request):
     if 'DocName' not in request.session:
@@ -50,24 +189,24 @@ def checker(request):
             myfile = request.FILES['DocFile']
             fs = FileSystemStorage()
             request.session['DocName'] = fs.save(myfile.name, myfile)
-            print(os.path.exists(os.getcwd() + '\\media\\' + request.session['DocName']))
+            #print(os.path.exists(os.getcwd() + '\\media\\' + request.session['DocName']))
         else:
             notDoc = True
         if 'HtmlFile' in request.FILES:
             myfile = request.FILES['HtmlFile']
             fs = FileSystemStorage()
             request.session['HtmlName'] = fs.save(myfile.name, myfile)
-            print(os.path.exists(os.getcwd() + '\\media\\' + request.session['HtmlName']))
+            #print(os.path.exists(os.getcwd() + '\\media\\' + request.session['HtmlName']))
         else:
             notHtml = True
         if 'DocType' in request.POST:
             request.session['DocType'] = request.POST['DocType']
-            print(request.session['DocType'])
+            #print(request.session['DocType'])
         if 'CustomTemplate' in request.FILES:
             myfile = request.FILES['CustomTemplate']
             fs = FileSystemStorage()
             request.session['CustomTemplate'] = fs.save(os.getcwd()+'\\media\\templates\\' + myfile.name, myfile)
-            print(os.path.exists(request.session['CustomTemplate']))
+            #print(os.path.exists(request.session['CustomTemplate']))
         else:
             if request.session['DocType'] == "2":
                 notWCT = True
@@ -80,11 +219,11 @@ def checker(request):
         arrayErr = []
         request.session['ErrorList'] = arrayErr
 
-        parse(os.getcwd() + '\\media\\' + request.session['DocName'], request.session['DocType'], request.session['CustomTemplate'])
+        parse(request, os.getcwd() + '\\media\\' + request.session['DocName'], os.getcwd() + '\\media\\' + request.session['HtmlName'], request.session['DocType'], request.session['CustomTemplate'])
 
-        request.session['ErrorList'].append(DocError(1, "Error1", "Error1Description", 3, 24, "").serialize())
-        request.session['ErrorList'].append(DocError(2, "Error2", "Error2Description", 5, 223, "").serialize())
-        request.session['ErrorList'].append(DocError(3, "Error3", "Error3Description", 14, 1, "").serialize())
+        #request.session['ErrorList'].append(DocError(1, "Error1", "Error1Description", "").serialize())
+        #request.session['ErrorList'].append(DocError(2, "Error2", "Error2Description", "").serialize())
+        #request.session['ErrorList'].append(DocError(3, "Error3", "Error3Description", "").serialize())
 
         #--------------------------------
 
@@ -281,9 +420,9 @@ def constructor(request):
 
         with open(os.getcwd()+'\\WCT\\'+request.session['TemplateName']+'.wct', 'rb') as fh:
             response = FileResponse(open(os.getcwd()+'\\WCT\\'+request.session['TemplateName']+'.wct', 'rb'))
-            if os.path.exist(os.getcwd()+'\\WCT\\'+request.session['TemplateName']+'.wct'):
-                os.remove(os.getcwd()+'\\WCT\\'+request.session['TemplateName']+'.wct')
             return response
+        #if os.path.exists(os.getcwd()+'\\WCT\\'+request.session['TemplateName']+'.wct'):
+            #os.remove(os.getcwd()+'\\WCT\\'+request.session['TemplateName']+'.wct')
 
     return render(request, 'constructor.html')
 
